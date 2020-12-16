58d405064221cd52d9f07668d6262296c12319e3
Juan Aldair Ruiz Carmona <aldair.rc70@gmail.com>
2020-12-16 14:11:09 -0500

    Variable de entorno MONGO

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ let urlDB = '';
 if (process.env.NODE_ENV === "dev") {
     urlDB = 'mongodb://localhost:27017/cafe_test';
 } else {
-    urlDB = 'mongodb+srv://Aldair619:@@@@S0p0rt3@cluster0.em3cz.mongodb.net/cafe_test';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
